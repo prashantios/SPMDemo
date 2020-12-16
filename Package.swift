729fe91451286dd813f079842eaf240a8358aecf
Prashant <prashant.aggarwal@berylsystems.com>
2020-12-16 22:34:56 +0530

    small change

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftSPM",
+    platforms: [.iOS(.v13), .macOS(.v10_14)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -24,7 +25,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftSPM",
-            dependencies: []),        
+            dependencies: []),
         .testTarget(
             name: "SwiftSPMTests",
             dependencies: ["SwiftSPM"]),
