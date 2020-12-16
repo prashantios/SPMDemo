7ebd69a1ba6771968825f5e80dd075882224a072
Prashant <prashant.aggarwal@berylsystems.com>
2020-12-16 22:29:33 +0530

    dec fixes

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftSPM",
-            dependencies: ["Alamofire", "SwiftyJSON","OpenSSL"]),        
+            dependencies: []),        
         .testTarget(
             name: "SwiftSPMTests",
             dependencies: ["SwiftSPM"]),
